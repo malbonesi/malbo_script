36280321da4486f1d84dccd3b2f0f5ef934a5538
malbonesi <mikealbo@gmail.com>
2013-11-25 22:06:32 +0000

    Update malbo_script.js

--- malbo_script.js
+++ malbo_script.js
@@ -3,7 +3,7 @@ API.on(API.CHAT, parseChat);
 
 function parseChat(data){
     var msg = data.message;
-    if (data.fromID = "50aeb40bd6e4a94f7747c5a4"){
+    if (data.fromID == "50aeb40bd6e4a94f7747c5a4"){
         if (msg.match(/-purr/)) { API.sendChat("Meow?"); }
     } 
 }
