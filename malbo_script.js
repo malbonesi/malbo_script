8bb55a92bf7ddc551b22adfa804554a3ce3b172e
malbonesi <mikealbo@gmail.com>
2013-11-26 00:02:42 +0000

    gui test

--- malbo_script.js
+++ malbo_script.js
@@ -1,6 +1,24 @@
 API.on(API.DJ_ADVANCE, function() { $("#woot").click() });
 API.on(API.CHAT, parseChat);
 
+var iconHTML = "<div id='bot-icon'>Darwin</div>";
+var menuHTML = "<div id='bot-menu'>Heyo</div>";
+
+$('#chat-header').append(menuHTML);
+$('#bot-icon').css({
+position: 'absolute',
+left: '192px',
+top: '8px',
+width: '110px',
+height: '30px',
+cursor: 'pointer',
+color: '#3B45A7',
+border-style: 'solid',
+border-wifth: '1px' 
+})
+
+$('#bot-icon').click(function() { $('#bot-menu').slideToggle('fast'); });
+
 function parseChat(data){
     var msg = data.message;
     if (data.fromID = "50aeb40bd6e4a94f7747c5a4"){
@@ -8,14 +26,12 @@ function parseChat(data){
     } 
 }
 
-var menuHTML = "<div id='bot-menu'>Darwin</div>";
-
-$('#chat-header').append(menuHTML);
-$('#bot-menu').css({
-position: 'absolute',
-left: '192px',
-top: '8px',
-width: '110px',
-height: '30px',
-cursor: 'pointer'
-})
+/*
+position:absolute;
+left:192px;
+width:110px;
+background:#111317;
+overflow:hidden;
+z-index:30000;
+display:none
+*/
