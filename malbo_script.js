64e00a90284246e30755f86f67313a14482bb9cc
malbonesi <mikealbo@gmail.com>
2013-12-08 20:55:24 +0000

    ui stuff

--- malbo_script.js
+++ malbo_script.js
@@ -4,7 +4,7 @@ API.on(API.CHAT, parseChat);
 
 //This is a simple UI
 var iconHTML = "<div id='bot-icon'>Darwin</div>";
-var menuHTML = "<div id='bot-menu'><ul id='bot-options'><li>Greet</li><li>Facts</li><li>Cap: <textarea rows='1' cols='2' /></li></ul></div>";
+var menuHTML = "<div id='bot-menu'><ul id='bot-options'><li>Greet</li><li>Facts</li><li>Cap: <textarea id='captext' rows='1' cols='2' /></li></ul></div>";
 
 $('#chat-header').append(iconHTML, menuHTML);
 $('#bot-icon').css({
@@ -39,6 +39,11 @@ zIndex: '30000'
 
 $('#bot-options').css({
 listStyle: 'none'
+});
+
+$('#textcap').css({
+resize: 'none',
+overflow: 'auto'
 });
 
 $('#bot-icon').click(function() { $('#bot-menu').slideToggle('fast'); });
