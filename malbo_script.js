cb5350cc5c8df15ac0cf6c2457fd19baf555f59f
malbonesi <mikealbo@gmail.com>
2013-12-18 01:53:50 +0000

    Update malbo_script.js

--- malbo_script.js
+++ malbo_script.js
@@ -56,7 +56,7 @@ $('#bot-icon').click(function() { $('#bot-menu').slideToggle('fast'); });
 
 function parseChat(data){
     var msg = data.message;
-    if (data.fromID = "50aeb40bd6e4a94f7747c5a4"){
+    if (data.fromID == "50aeb40bd6e4a94f7747c5a4"){
         if (msg.match(/-purr/)) { API.sendChat("Meow?"); }
     } 
 }
