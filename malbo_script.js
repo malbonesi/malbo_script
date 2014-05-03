e0a4ae0c9e978c97bec49f60fa8f7d126c8452d5
malbonesi <mikealbo@gmail.com>
2014-05-03 17:22:17 +0000

    Update malbo_script.js

--- malbo_script.js
+++ malbo_script.js
@@ -1,3 +1,7 @@
+$("#playback-container").css("visibility", "hidden");
+$(".background").css("visibility", "hidden");
+$("#no-dj").css("visibility", "hidden");
+
 API.on(API.DJ_ADVANCE, function() { $("#woot").click(); });
 API.on(API.CHAT, parseChat);
 
