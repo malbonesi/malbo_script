62b219b01b42a474238a9ffb0589bccbb337cfe3
malbonesi <mikealbo@gmail.com>
2014-08-31 03:23:56 +0000

    Update malbo_script.js

--- malbo_script.js
+++ malbo_script.js
@@ -1,15 +1,16 @@
-$("#playback-container, .background, #no-dj").css("visibility", "hidden");
+$('#playback-container, .background, #no-dj').css('visibility', 'hidden');
+$('playback').off('mouseenter');
 //$(".background").css("visibility", "hidden");
 //$("#no-dj").css("visibility", "hidden");
 
-API.on(API.DJ_ADVANCE, function() { $("#woot").click(); });
+API.on(API.DJ_ADVANCE, function() { $('#woot').click(); });
 API.on(API.CHAT, parseChat);
 
 function parseChat(data){
 
     var msg = data.message;
     
-    if (data.fromID == "50aeb40bd6e4a94f7747c5a4"){   
-        if (msg == "-purr") { API.sendChat("Meow?"); }
+    if (data.fromID == '50aeb40bd6e4a94f7747c5a4'){   
+        if (msg == '-purr') { API.sendChat('Meow?'); }
     }
 }
