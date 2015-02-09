fd11b1f727ce097b55a9118f13f2caeacd91c635
malbonesi <mikealbo@gmail.com>
2015-02-09 07:46:20 +0000

    Update plugscript.js

--- plugscript.js
+++ plugscript.js
@@ -1,23 +1,7 @@
+//Hide video player, yet keep playback, and remove mouseenter event for visual consistency
 $('#playback-container, .background, #no-dj').css('visibility', 'hidden');
 $('#playback').off('mouseenter');
-//$('#chat-button, #users-button').off('click');
-//Add app-left to include user-list
-//Can probably piggy-back on whatever is used for responsive design. Just take the height and stuff from app-right
-//Should probably set up element before hand in an object or something?
-//$('.app-right').before("<div id='app-left'></div>");
-//$('#app-left').css('border', '1px solid black');
-//$('#user-lists').appendTo('#app-left').css({'display':'block','visibility':'visible'});
 
+//Autowoot when dj advances
 API.on(API.ADVANCE, woot);
-API.on(API.DJ_ADVANCE, woot);
-API.on(API.CHAT, parseChat);
-
 function woot(){ $('#woot').click(); }
-function parseChat(data){
-
-    var msg = data.message;
-    
-    if (data.fromID == '50aeb40bd6e4a94f7747c5a4'){   
-        if (msg == '-purr') { API.sendChat('Meow?'); }
-    }
-}
